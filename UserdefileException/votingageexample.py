074f1d564774bcf8f6da317f9f50e6051aaed0f3
Riya-1799 <riyaarvadia178@gmail.com>
2024-11-23 11:52:43 -0500

    voting example for raise user define exception is done..

---
+++ UserdefileException/votingageexample.py
@@ -0,0 +1,28 @@
+class MiniorException(Exception):
+    pass
+class person:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+
+    def get_minior_age(self):
+        if int(self.age) <= 18:
+            raise MiniorException
+        else:
+            print(f"the age of {self.name} is {self.age}, can give their vote")
+
+    def display(self):
+        try:
+            self.get_minior_age()
+        except MiniorException:
+            print(f"{self.name} is not an adult, cannot be vote")
+        finally:
+            pass
+
+# No exception
+person("Bhavin", 17).display()
+
+# AdultException is raised
+person("Dhaval", 23).display()
+
